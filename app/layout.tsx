f9fff0ab42236a8b83d1fb6d6a4739229ce8978f
Akash M Kumar <akashmkumar1@gmail.com>
2024-05-25 23:37:48 +0530

    commit one
    bare bones of banking application

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({ 
+  subsets: ["latin"],
+  weight: ['400','700'], 
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "VistaLuxe",
+  description: " VistaLuxe elevates your financial journey with its luxurious, clear, and user-friendly services. Enjoy top-tier tools for tracking spending, optimizing savings, and making informed investments.",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
