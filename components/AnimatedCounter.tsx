3d5dcfedca1f5f94b3f35e32e2cc9d0b90c3f143
Akash M Kumar <akashmkumar1@gmail.com>
2024-05-26 01:02:29 +0530

    update 1
    added charts and animated counter features

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,15 @@
+'use client'
+import CountUp from 'react-countup'
+const AnimatedCounter = ({amount}: {amount: number}) => {
+  return (
+    <div>
+        <CountUp 
+         decimals={2}
+         decimal="."
+         prefix="₹"
+        end={amount} />
+    </div>
+  )
+}
+
+export default AnimatedCounter
