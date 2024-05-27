59c85606662251ad7ccb1b36e2d170a6a7040699
Akash M Kumar <akashmkumar1@gmail.com>
2024-05-27 22:32:30 +0530

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,15 +6,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex min-h-screen w-full justify-between font-inter">
+    <main className="flex min-h-screen w-full  justify-between font-inter">
       {children}
       <div className="auth-asset">
         <div>
           <Image 
             src="/icons/auth-image.svg"
             alt="Auth image"
-            width={500}
-            height={500}
+            width={5000}
+            height={5000}
             className="rounded-l-xl object-contain"
           />
         </div>
