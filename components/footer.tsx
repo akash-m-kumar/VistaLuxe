2a46be05b26185a04ae9eaeb5b8cb37df107115e
Akash M Kumar <akashmkumar1@gmail.com>
2024-05-27 22:27:28 +0530

    auth-image changed

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
+
 const Footer = ({ user, type = 'desktop' }: FooterProps) => {
   const router = useRouter();
 
