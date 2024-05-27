db6b88a05a1ce14ba7ba1445466b41a7ef0f2336
Akash M Kumar <akashmkumar1@gmail.com>
2024-05-27 20:52:36 +0530

    updated imports

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,7 +14,7 @@ import { cn } from "@/lib/utils"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import Footer from "@/components/Footer"
+import Footer from "../components/Footer"
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
