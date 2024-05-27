01094ee475707607a76501dc06557ab2e54fab92
Akash M Kumar <akashmkumar1@gmail.com>
2024-05-27 22:20:01 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import PlaidLink from './PlaidLink'
 import Footer from './Footer'
-//
+///
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
